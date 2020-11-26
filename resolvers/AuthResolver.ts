e7987e8b5022d731990b5fd3c6c166a22c404b29
Neezar <rforlyffe@gmaicl.com>
2020-11-27 01:03:11 +0800

    add register mutation

---
+++ resolvers/AuthResolver.ts
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import { Arg, Mutation, Resolver } from "type-graphql";
+import { UserResponse } from "../types/UserResponse";
+import { AuthInput } from "../types/AuthInput";
+import { UserModel } from "../entity/User";
+
+@Resolver()
+export class AuthResolver {
+  @Mutation(() => UserResponse)
+  async register(
+    @Arg("input") { email, password }: AuthInput
+  ): Promise<UserResponse> {
+    const existingUser = await UserModel.findOne({ email });
+
+    if (!existingUser) {
+      throw new Error("Email already in use");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new UserModel({ email, password: hashedPassword });
+    await user.save();
+
+    const payload = {
+      id: user.id,
+    };
+
+    const token = jwt.sign(
+      payload,
+      process.env.SESSION_SECRET || "umajolieanarosecharity"
+    );
+
+    return { user, token };
+  }
+}
